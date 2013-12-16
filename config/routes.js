e5de105df0f4a8851f155e95eb01951d4de3c63c
Mark Davies <mark@synthmedia.co.uk>
2013-12-16 16:41:38 +0000

    first commit

--- config/routes.js
+++ config/routes.js
@@ -2,18 +2,26 @@ module.exports = function(app){
 
 	//home route
 	var home = require('../app/controllers/home');
-	app.get('/', home.index);
-
-
+	app.get('/', function (req, res) {
+   
+    res.render('home/index', {
+      title: 'Generator-Express MVC',
+      checkIns : app.checkins,
+      articles: articles
+    });
+  });
   //home route
   app.post('/check-in', function(req, res){
     app.checkins = app.checkins + 1;
   console.log('Checked in, Check ins :  ' + app.checkins);
   });
   app.post('/check-out', function(req, res){
-        app.checkins = app.checkins - 1;
 
-  console.log('Checked out, Check ins :  ' + app.checkins);
+    if (app.checkins > 0 ){
+      app.checkins = app.checkins - 1;
+    }
+
+   console.log('Checked out, Check ins :  ' + app.checkins);
   });
 
 };
