aa30c1b7c369dff1987b24e8afe14bc8387aec94
Mark Davies <mark@synthmedia.co.uk>
2013-12-16 16:34:44 +0000

    first commit

--- config/routes.js
+++ config/routes.js
@@ -9,11 +9,11 @@ module.exports = function(app){
   app.post('/check-in', function(req, res){
     app.checkins = app.checkins + 1;
   console.log('Checked in, Check ins :  ' + app.checkins);
-  }
+  });
   app.post('/check-out', function(req, res){
         app.checkins = app.checkins - 1;
 
   console.log('Checked out, Check ins :  ' + app.checkins);
-  }
+  });
 
 };
